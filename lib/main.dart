47a7bd0eb5f1a4407f00328f315a69e4759a3fda
mdshahsamir <mdshahsamir@gmail.com>
2020-12-06 16:29:27 +0600

    All course tasks done in this commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,9 +16,46 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftImageAsset = 1;
+  int rightImageAsset = 1;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              child: Image.asset('images/dice$leftImageAsset.png'),
+              onPressed: () {
+                setState(() {
+                  diceStateGenerator();
+                });
+              },
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              child: Image.asset('images/dice$rightImageAsset.png'),
+              onPressed: () {
+                setState(() {
+                  diceStateGenerator();
+                });
+              },
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  void diceStateGenerator() {
+    leftImageAsset = Random().nextInt(6) + 1;
+    rightImageAsset = Random().nextInt(6) + 1;
   }
 }
